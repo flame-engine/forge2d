02103c7a98636360506240832677507f0975fadc
Kevin Moore <kevmoo@google.com>
2015-05-08 12:02:39 -0700

    Add Travis and code coverage support
    R=johnmccutchan@google.com
    
    Review URL: https://codereview.chromium.org//1132123002

---
+++ tool/travis.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Fast fail the script on failures.   
+set -e
+
+dartanalyzer lib/box2d.dart
+
+# Run the benchmark script as a simple sanity check
+dart --checked --error_on_bad_type --error_on_bad_override benchmark/bench2d.dart
+
+# Install dart_coveralls; gather and send coverage data.
+if [ "$COVERALLS_TOKEN" ] && [ "$TRAVIS_DART_VERSION" = "stable" ]; then
+  pub global activate dart_coveralls
+  pub global run dart_coveralls report \
+    --token $COVERALLS_TOKEN \
+    --retry 2 \
+    --exclude-test-files \
+    benchmark/bench2d.dart
+fi
