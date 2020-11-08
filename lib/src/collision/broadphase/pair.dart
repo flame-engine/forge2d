6b3115f9e38ca5c786f4eeac406fa7e305d80ede
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2020-11-08 23:32:35 +0100

    Fix pair compareTo

--- lib/src/collision/broadphase/pair.dart
+++ lib/src/collision/broadphase/pair.dart
@@ -1,7 +1,5 @@
 part of forge2d;
 
-/// Java note: at the "creation" of each node, a random key is given to that node, and that's what we
-/// sort from.
 class Pair implements Comparable<Pair> {
   int proxyIdA = 0;
   int proxyIdB = 0;
@@ -11,15 +9,9 @@ class Pair implements Comparable<Pair> {
     if (proxyIdA < pair2.proxyIdA) {
       return -1;
     }
-
     if (proxyIdA == pair2.proxyIdA) {
-      return proxyIdB < pair2.proxyIdB
-          ? -1
-          : proxyIdB == pair2.proxyIdB
-              ? 0
-              : 1;
+      return proxyIdB.compareTo(pair2.proxyIdB);
     }
-
     return 1;
   }
 }
