edd982abbd78be489903fa89a1f5292fad2c2473
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2020-11-10 19:24:00 +0100

    Re-add pools for collision distance

--- lib/src/collision/broadphase/default_broadphase_buffer.dart
+++ lib/src/collision/broadphase/default_broadphase_buffer.dart
@@ -115,7 +115,7 @@ class DefaultBroadPhaseBuffer implements TreeCallback, BroadPhase {
     _moveCount = 0;
 
     // Sort the pair buffer to expose duplicates.
-    _pairBuffer.sort();
+    buffer_utils.sort(_pairBuffer, 0, _pairCount);
 
     // Send the pairs back to the client.
     int i = 0;
