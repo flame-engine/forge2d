1e13773bffe760c8e4ae1d6601110f5fb894ec4e
alestiago <dev@alestiago.com>
2022-03-15 15:42:56 +0000

    feat: included RopeJointDef generics

--- lib/src/dynamics/joints/rope_joint_def.dart
+++ lib/src/dynamics/joints/rope_joint_def.dart
@@ -2,7 +2,7 @@ import '../../../forge2d.dart';
 
 /// Rope joint definition. This requires two body anchor points and a maximum lengths. Note: by
 /// default the connected objects will not collide. see collideConnected in b2JointDef.
-class RopeJointDef extends JointDef {
+class RopeJointDef<A extends Body, B extends Body> extends JointDef<A, B> {
   /// The maximum length of the rope. Warning: this must be larger than linearSlop or the joint
   /// will have no effect.
   double maxLength = 0.0;
