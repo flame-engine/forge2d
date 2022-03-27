4d96c7a101655e3b1b388b8a9dcb77c46b663c47
alestiago <dev@alestiago.com>
2022-03-27 17:01:40 +0100

    feat: included tests

---
+++ packages/forge2d/test/dynamics/world_test.dart
@@ -0,0 +1,41 @@
+import 'package:forge2d/forge2d.dart';
+import 'package:test/expect.dart';
+import 'package:test/scaffolding.dart';
+
+class AnotherRevoluteJoint extends RevoluteJoint {
+  AnotherRevoluteJoint(RevoluteJointDef<Body, Body> def) : super(def);
+}
+
+void main() {
+  group(
+    'addJoint',
+    () {
+      // TODO(alesitiago): Consider improving test and testing for all other
+      // Joints.
+
+      test('adds RevoluteJoint to joints', () {
+        final world = World();
+        expect(world.joints.isEmpty, isTrue);
+
+        final joint = RevoluteJoint(
+          RevoluteJointDef()
+            ..bodyA = Body(BodyDef(), world)
+            ..bodyB = Body(BodyDef(), world),
+        );
+        world.addJoint(joint);
+
+        expect(world.joints.first, joint);
+      });
+
+      test('supports adding RevoluteJoint subclasses', () {
+        final world = World();
+        final joint = AnotherRevoluteJoint(
+          RevoluteJointDef()
+            ..bodyA = Body(BodyDef(), world)
+            ..bodyB = Body(BodyDef(), world),
+        );
+        expect(() => world.addJoint(joint), returnsNormally);
+      });
+    },
+  );
+}
