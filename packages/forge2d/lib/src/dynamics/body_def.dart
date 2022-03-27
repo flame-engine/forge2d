a11666dfa9b122ac6c0f826b83ae55985e5ee54e
alestiago <dev@alestiago.com>
2022-03-27 18:34:16 +0100

    docs: applied EffectiveDart formatting

--- packages/forge2d/lib/src/dynamics/body_def.dart
+++ packages/forge2d/lib/src/dynamics/body_def.dart
@@ -1,7 +1,11 @@
 import '../../forge2d.dart';
 
-/// A body definition holds all the data needed to construct a rigid body. You can safely re-use body
-/// definitions. Shapes are added to a body after construction.
+/// Holds all the data needed to construct a [Body].
+///
+/// You can safely re-use body definitions.
+///
+/// [Shape]s are added through [Fixture]s to a [Body] after construction via
+/// [Body.createFixture].
 class BodyDef {
   BodyDef({
     this.type = BodyType.static,
@@ -21,15 +25,18 @@ class BodyDef {
   })  : position = position ?? Vector2.zero(),
         linearVelocity = linearVelocity ?? Vector2.zero();
 
-  /// The body type: static, kinematic, or dynamic. Note: if a dynamic body would have zero mass, the
-  /// mass is set to one.
+  /// The body type: static, kinematic, or dynamic.
+  ///
+  /// Note: A [BodyType.dynamic] body with zero mass, will have a mass of one.
   BodyType type;
 
   /// Use this to store application specific body data.
   Object? userData;
 
-  /// The world position of the body. Avoid creating bodies at the origin since this can lead to many
-  /// overlapping shapes.
+  /// The world position of the body.
+  ///
+  /// Avoid creating bodies at the origin since this can lead to many
+  /// overlapping [Shape]s.
   Vector2 position;
 
   /// The world angle of the body in radians.
@@ -41,31 +48,40 @@ class BodyDef {
   /// The angular velocity of the body.
   double angularVelocity;
 
-  /// Linear damping is use to reduce the linear velocity. The damping parameter can be larger than
-  /// 1.0f but the damping effect becomes sensitive to the time step when the damping parameter is
-  /// large.
+  /// Linear damping is use to reduce the linear velocity.
+  ///
+  /// The damping parameter can be larger than 1.0f but the damping effect
+  /// becomes sensitive to the time step when the damping parameter is large.
   double linearDamping;
 
-  /// Angular damping is use to reduce the angular velocity. The damping parameter can be larger than
-  /// 1.0f but the damping effect becomes sensitive to the time step when the damping parameter is
-  /// large.
+  /// Angular damping is use to reduce the angular velocity.
+  ///
+  /// The damping parameter can be larger than 1.0f but the damping effect
+  /// becomes sensitive to the time step when the damping parameter is large.
   double angularDamping;
 
-  /// Set this flag to false if this body should never fall asleep. Note that this increases CPU
-  /// usage.
+  /// Set this flag to false if this body should never fall asleep.
+  ///
+  /// Note: Not alllowing a body to sleep increases CPU usage.
   bool allowSleep;
 
   /// Is this body initially sleeping?
   bool isAwake;
 
-  /// Should this body be prevented from rotating? Useful for characters.
+  /// Should this body be prevented from rotating?
+  ///
+  /// Useful for characters.
   bool fixedRotation;
 
-  /// Is this a fast moving body that should be prevented from tunneling through other moving bodies?
-  /// Note that all bodies are prevented from tunneling through kinematic and static bodies. This
-  /// setting is only considered on dynamic bodies.
+  /// Is this a fast moving body that should be prevented from tunneling through
+  /// other moving bodies?
   ///
-  /// @warning You should use this flag sparingly since it increases processing time.
+  /// Note: All bodies are prevented from tunneling through [BodyType.kinematic]
+  /// and [BodyType.static] bodies. This setting is only considered on
+  /// [BodyType.dynamic] bodies.
+  ///
+  /// Warning: You should use this flag sparingly since it increases processing
+  /// time.
   bool bullet;
 
   /// Does this body start out active?
--- packages/forge2d/lib/src/dynamics/body_type.dart
+++ packages/forge2d/lib/src/dynamics/body_type.dart
@@ -1,5 +1,17 @@
-/// The body type.
-/// static: zero mass, zero velocity, may be manually moved
-/// kinematic: zero mass, non-zero velocity set by user, moved by solver
-/// dynamic: positive mass, non-zero velocity determined by forces, moved by solver
-enum BodyType { static, kinematic, dynamic }
+import '../../forge2d.dart';
+
+/// {@template dynamics.body_type}
+/// Defines the type of a [Body].
+/// {@endtemplate}
+enum BodyType {
+  /// Defines a [Body] with zero mass, zero velocity, may be manually moved.
+  static,
+
+  /// Defines a [Body] with zero mass, non-zero velocity set by user, moved by
+  /// solver.
+  kinematic,
+
+  /// Defines a [Body] with positive mass, non-zero velocity determined by
+  /// forces, moved by solver.
+  dynamic,
+}
