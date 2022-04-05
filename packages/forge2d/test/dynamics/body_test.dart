09a9cb360da7a2a3394a51dec2d3c9893ccc0ffb
alestiago <dev@alestiago.com>
2022-04-05 21:10:05 +0100

    feat: included tests

--- packages/forge2d/test/dynamics/body_test.dart
+++ packages/forge2d/test/dynamics/body_test.dart
@@ -39,5 +39,41 @@ void main() {
         expect(bodyInitialPosition.y, isNot(equals(body.position.y)));
       });
     });
+
+    group('gravityScale', () {
+      test("body doesn't move when is zero", () {
+        final gravity = Vector2(10, 10);
+        final world = World(gravity);
+
+        final body = world.createBody(
+          BodyDef()
+            ..type = BodyType.dynamic
+            ..gravityScale = Vector2.zero(),
+        );
+
+        final bodyInitialPosition = body.position.clone();
+
+        world.stepDt(1);
+
+        expect(bodyInitialPosition.x, equals(body.position.x));
+        expect(bodyInitialPosition.y, equals(body.position.y));
+      });
+
+      test(
+          "body moves with world's gravity "
+          'when gravityScale is not specfied', () {
+        final gravity = Vector2(10, 10);
+        final world = World(gravity);
+
+        final body = world.createBody(BodyDef()..type = BodyType.dynamic);
+
+        final bodyInitialPosition = body.position.clone();
+
+        world.stepDt(1);
+
+        expect(bodyInitialPosition.x, isNot(equals(body.position.x)));
+        expect(bodyInitialPosition.y, isNot(equals(body.position.y)));
+      });
+    });
   });
 }
