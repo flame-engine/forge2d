2ac54317ee5f04559cd4077e6623d69e715bf6ee
alestiago <dev@alestiago.com>
2022-05-13 15:11:44 +0100

    test: included waking tests

--- packages/forge2d/test/dynamics/joints/prismatic_joint_test.dart
+++ packages/forge2d/test/dynamics/joints/prismatic_joint_test.dart
@@ -2,7 +2,9 @@ import 'package:forge2d/forge2d.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
 
-import '../../helpers/helpers.dart';
+class _MockBody extends Mock implements Body {}
+
+class _MockDebugDraw extends Mock implements DebugDraw {}
 
 void main() {
   group('PrismaticJoint', () {
@@ -10,10 +12,9 @@ void main() {
 
     setUp(
       () {
-        final world = World();
         jointDef = PrismaticJointDef()
-          ..bodyA = Body(BodyDef(), world)
-          ..bodyB = Body(BodyDef(), world);
+          ..bodyA = _MockBody()
+          ..bodyB = _MockBody();
       },
     );
 
@@ -21,21 +22,31 @@ void main() {
       expect(PrismaticJoint(jointDef), isA<PrismaticJoint>());
     });
 
-    test('can change motor speed', () {
-      final joint = PrismaticJoint(jointDef);
+    group('motorSpeed', () {
+      test('can change motor speed', () {
+        final joint = PrismaticJoint(jointDef);
 
-      final oldMotorSpeed = joint.motorSpeed;
-      final newMotorSpeed = oldMotorSpeed + 1;
-      joint.motorSpeed = newMotorSpeed;
+        final oldMotorSpeed = joint.motorSpeed;
+        final newMotorSpeed = oldMotorSpeed + 1;
+        joint.motorSpeed = newMotorSpeed;
 
-      expect(joint.motorSpeed, equals(newMotorSpeed));
+        expect(joint.motorSpeed, equals(newMotorSpeed));
+      });
+
+      test('wakes up both bodies', () {
+        final joint = PrismaticJoint(jointDef);
+        joint.motorSpeed = 1;
+
+        verify(() => joint.bodyA.setAwake(true)).called(1);
+        verify(() => joint.bodyB.setAwake(true)).called(1);
+      });
     });
 
     group('render', () {
       late DebugDraw debugDraw;
 
       setUp(() {
-        debugDraw = MockDebugDraw();
+        debugDraw = _MockDebugDraw();
 
         registerFallbackValue(Vector2.zero());
         registerFallbackValue(Color3i.black);
@@ -43,6 +54,11 @@ void main() {
 
       test('draws three segments', () {
         final joint = PrismaticJoint(jointDef);
+        when(() => joint.bodyA.transform).thenReturn(Transform.zero());
+        when(() => joint.bodyB.transform).thenReturn(Transform.zero());
+        when(() => joint.bodyA.worldPoint(any())).thenReturn(Vector2.zero());
+        when(() => joint.bodyB.worldPoint(any())).thenReturn(Vector2.zero());
+
         joint.render(debugDraw);
         verify(() => debugDraw.drawSegment(any(), any(), any())).called(3);
       });
--- packages/forge2d/test/dynamics/joints/wheel_joint_test.dart
+++ packages/forge2d/test/dynamics/joints/wheel_joint_test.dart
@@ -2,38 +2,49 @@ import 'package:forge2d/forge2d.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
 
-import '../../helpers/helpers.dart';
+class _MockBody extends Mock implements Body {}
+
+class _MockDebugDraw extends Mock implements DebugDraw {}
 
 void main() {
   group('WheelJoint', () {
     late WheelJointDef jointDef;
 
     setUp(() {
-      final world = World();
       jointDef = WheelJointDef()
-        ..bodyA = Body(BodyDef(), world)
-        ..bodyB = Body(BodyDef(), world);
+        ..bodyA = _MockBody()
+        ..bodyB = _MockBody();
     });
 
     test('can be instantiated', () {
       expect(WheelJoint(jointDef), isA<WheelJoint>());
     });
 
-    test('can change motor speed', () {
-      final joint = WheelJoint(jointDef);
+    group('motorSpeed', () {
+      test('can change motor speed', () {
+        final joint = WheelJoint(jointDef);
 
-      final oldMotorSpeed = joint.motorSpeed;
-      final newMotorSpeed = oldMotorSpeed + 1;
-      joint.motorSpeed = newMotorSpeed;
+        final oldMotorSpeed = joint.motorSpeed;
+        final newMotorSpeed = oldMotorSpeed + 1;
+        joint.motorSpeed = newMotorSpeed;
 
-      expect(joint.motorSpeed, equals(newMotorSpeed));
+        expect(joint.motorSpeed, equals(newMotorSpeed));
+      });
+
+      test('wakes up both bodies', () {
+        final joint = WheelJoint(jointDef);
+        joint.motorSpeed = 1;
+
+        verify(() => joint.bodyA.setAwake(true)).called(1);
+        verify(() => joint.bodyB.setAwake(true)).called(1);
+      });
     });
 
     group('render', () {
       late DebugDraw debugDraw;
 
       setUp(() {
-        debugDraw = MockDebugDraw();
+        debugDraw = _MockDebugDraw();
 
         registerFallbackValue(Vector2.zero());
         registerFallbackValue(Color3i.black);
@@ -41,6 +52,11 @@ void main() {
 
       test('draws three segments', () {
         final joint = WheelJoint(jointDef);
+        when(() => joint.bodyA.transform).thenReturn(Transform.zero());
+        when(() => joint.bodyB.transform).thenReturn(Transform.zero());
+        when(() => joint.bodyA.worldPoint(any())).thenReturn(Vector2.zero());
+        when(() => joint.bodyB.worldPoint(any())).thenReturn(Vector2.zero());
+
         joint.render(debugDraw);
         verify(() => debugDraw.drawSegment(any(), any(), any())).called(3);
       });
