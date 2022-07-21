c3967ce880f5c9d0dfa68e0a29284818109d0150
alestiago <dev@alestiago.com>
2022-07-21 10:37:15 +0100

    test: added bullet change tests

--- packages/forge2d/test/dynamics/body_test.dart
+++ packages/forge2d/test/dynamics/body_test.dart
@@ -1,9 +1,22 @@
 import 'package:forge2d/forge2d.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:test/expect.dart';
 import 'package:test/scaffolding.dart';
 
 void main() {
   group('Body', () {
+    group('bullet', () {
+      test('can change', () {
+        final body = Body(BodyDef(), World());
+        const newBulletValue = true;
+        expect(body.bullet, isNot(equals(newBulletValue)));
+
+        body.bullet = newBulletValue;
+
+        expect(body.bullet, equals(newBulletValue));
+      });
+    });
+
     group('gravityOverride', () {
       test("body doesn't move when is zero", () {
         final gravity = Vector2(10, 10);
